c816278a25f36ae0f334a6bccdbe95bb10781794
Steve Lievens <steve.lievens@be.ibm.com>
2023-02-15 10:23:25 +0100

    adding native build

--- src/main/java/org/ibm/ce/quarkus/starting/BookRepository.java
+++ src/main/java/org/ibm/ce/quarkus/starting/BookRepository.java
@@ -2,6 +2,8 @@ package org.ibm.ce.quarkus.starting;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -13,13 +15,16 @@ public class BookRepository {
     @Inject
     Logger logger;
 
+    @ConfigProperty(name = "books.genre", defaultValue = "Sci-Fi")
+    String genre;
+
     public List<Book> getAllBooks() {
         logger.info("Returning all books");
         return List.of(
-                new Book(1, "Understanding Quarkus", "Antonio", 2021, "IT"),
-                new Book(2, "Practising Quarkus", "Antonio", 2020, "IT"),
-                new Book(3, "Effective Java", "Josh Bloch", 2001, "IT"),
-                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, "IT"));
+                new Book(1, "Understanding Quarkus", "Antonio", 2021, genre),
+                new Book(2, "Practising Quarkus", "Antonio", 2020, genre),
+                new Book(3, "Effective Java", "Josh Bloch", 2001, genre),
+                new Book(4, "Thinking in Java", "Bruce Eckel", 1998, genre));
     }
 
     public Optional<Book> getBook(int id) {
--- src/test/java/org/ibm/ce/quarkus/starting/BookResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.ibm.ce.quarkus.starting;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
-}
